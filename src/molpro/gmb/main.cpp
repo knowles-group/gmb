d4dc9c7b8163b0fb7645a44b25339e96c678ab12
Marta <mrtlvdl@gmail.com>
2021-05-12 14:12:19 +0100

    initial guess

--- src/molpro/gmb/main.cpp
+++ src/molpro/gmb/main.cpp
@@ -24,9 +24,8 @@ int main(int argc, char const *argv[])
   filename += "/" + test_case + ".fcidump";
   auto start = std::chrono::system_clock::now();
 
-  // std::unique_ptr<hamiltonian<>> hamiltonian;
   hamiltonian<> hamiltonian;
-  amplitudes<> t_ampl;
+  std::unique_ptr<amplitudes<>> ptampl(new amplitudes());
   std::unique_ptr<problem_gen> problem;
   std::string method_gs, method_es;
 
@@ -39,63 +38,35 @@ int main(int argc, char const *argv[])
 
   // parse data
   if (method_gs == "CCSD" or method_gs == "CCS")
-    t_ampl.set(t1, hamiltonian.m2get(f_ov));
+    ptampl->set(t1, container(hamiltonian.m2get(f_ov).get_space()));
   if (method_gs == "CCSD" or method_gs == "CCD")
-    t_ampl.set(t2, hamiltonian.m4get(i_oovv));
+    ptampl->set(t2, container(hamiltonian.m4get(i_oovv).get_space()));
   if (method_gs == "CCSD")
     problem.reset(new problem_ccsd(hamiltonian));
   if (method_gs == "CCD")
     problem.reset(new problem_ccd(hamiltonian));
 
-  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<supercontainer<>>("DIIS");
+  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<amplitudes<>>("DIIS");
 
-  auto residual = t_ampl;
+  auto residual = *ptampl;
   solver->set_verbosity(molpro::linalg::itsolv::Verbosity::None);
-  solver->solve(t_ampl, residual, *problem);
-  solver->solution(t_ampl, residual);
-  problem->energy(t_ampl);
+  solver->solve(*ptampl, residual, *problem);
+  solver->solution(*ptampl, residual);
+  problem->energy(*ptampl);
   std::cout << method_gs << " energy: " << std::setprecision(8) << problem->get_energy() << std::endl;
 
   #if 1// Excited State
   std::cout << std::endl << method_es << std::endl;
-  size_t nroots(2);
-  supercontainer<> r_ampl;
-  std::vector<supercontainer<>> v_rampl(nroots);
-  std::vector<container<2>> vr1;
+  size_t nroots(4);
+  std::vector<amplitudes<>> v_rampl(nroots);
   std::unique_ptr<problem_eom> problem_es;
-  for (size_t ir = 0; ir < nroots; ir++) {
-
-    container r1(hamiltonian.m2get(f_ov).get_space());
-    bbo::zero(r1);
-    libtensor::block_tensor_wr_ctrl<2, double> ctrl(r1);
-    libtensor::orbit_list<2, double> ol(ctrl.req_const_symmetry());
-    for (libtensor::orbit_list<2, double>::iterator it = ol.begin();
-         it != ol.end(); it++) {
-      libtensor::index<2> bidx;
-      ol.get_index(it, bidx);
-      libtensor::dense_tensor_wr_i<2, double> &blk = ctrl.req_block(bidx);
-      libtensor::dense_tensor_wr_ctrl<2, double> tc(blk);
-      const libtensor::dimensions<2> &tdims = blk.get_dims();
-      double *ptr = tc.req_dataptr();
-      if (ir == 1)
-        if (bidx[0] != 1 or bidx[1] != 1) continue;
-      ptr[0] = 1;
-      tc.ret_dataptr(ptr);
-      ctrl.ret_block(bidx);
-      break;
-    }
-    v_rampl[ir].set("r1", r1);
-    container r2(hamiltonian.m4get(i_oovv).get_space());
-    bbo::zero(r2);
-    v_rampl[ir].set("r2", r2);
-  }
   
-  problem_es.reset(new problem_eom_ccsd(hamiltonian, t_ampl));
+  problem_es.reset(new problem_eom_ccsd(hamiltonian, *ptampl));
 
-  auto solver_es = molpro::linalg::itsolv::create_LinearEigensystem<supercontainer<>>("Davidson");
+  auto solver_es = molpro::linalg::itsolv::create_LinearEigensystem<amplitudes<>>("Davidson");
   auto residuals_es = v_rampl;
   solver_es->set_n_roots(nroots);
-  solver_es->solve(v_rampl, residuals_es, *problem_es, false);
+  solver_es->solve(v_rampl, residuals_es, *problem_es, true);
 #endif
 
   auto end = std::chrono::system_clock::now();
@@ -106,4 +77,4 @@ int main(int argc, char const *argv[])
 }
 
 #include <molpro/linalg/itsolv/SolverFactory-implementation.h>
-template class molpro::linalg::itsolv::SolverFactory<supercontainer<>, supercontainer<>>;
+template class molpro::linalg::itsolv::SolverFactory<amplitudes<>, amplitudes<>>;
--- src/molpro/gmb/problem_ccsd.h
+++ src/molpro/gmb/problem_ccsd.h
@@ -16,10 +16,10 @@ public:
                     const std::vector<value_t> &shift) const override {
     for (int k = 0; k < g.size(); k++) {
       auto &a = g[k].get();     
-      auto t1_new = update_t1((m_ham.m2get(f_oo)), (m_ham.m2get(f_vv)), a.m2get("t1"));
-      auto t2_new = update_t2(m_ham.m2get(f_oo), m_ham.m2get(f_vv), a.m4get("t2"));
-      a.set("t1", t1_new);
-      a.set("t2", t2_new);
+      auto t1_new = update_t1((m_ham.m2get(f_oo)), (m_ham.m2get(f_vv)), a.m2get(t1));
+      auto t2_new = update_t2(m_ham.m2get(f_oo), m_ham.m2get(f_vv), a.m4get(t2));
+      a.set(t1, t1_new);
+      a.set(t2, t2_new);
     }
   }
 
@@ -28,30 +28,30 @@ public:
     value_t value = 0;
     auto &ccv = const_cast<container_t&> (v);        
 
-    auto if_oo = ccsd_if_oo(ccv.m2get("t1"), ccv.m4get("t2"), m_ham.m2get(f_oo),
+    auto if_oo = ccsd_if_oo(ccv.m2get(t1), ccv.m4get(t2), m_ham.m2get(f_oo),
                    m_ham.m4get(i_ooov), m_ham.m4get(i_oovv));    
-    auto if_ov = ccsd_if_ov(ccv.m2get("t1"), m_ham.m2get(f_ov), m_ham.m4get(i_oovv)); 
-    auto if_vv = ccsd_if_vv(ccv.m2get("t1"), ccv.m4get("t2"), m_ham.m2get(f_ov), m_ham.m2get(f_vv), 
+    auto if_ov = ccsd_if_ov(ccv.m2get(t1), m_ham.m2get(f_ov), m_ham.m4get(i_oovv)); 
+    auto if_vv = ccsd_if_vv(ccv.m2get(t1), ccv.m4get(t2), m_ham.m2get(f_ov), m_ham.m2get(f_vv), 
                    m_ham.m4get(i_oovv), m_ham.m4get(i_ovvv));
 
-    auto if2_oo = ccsd_if2_oo(ccv.m2get("t1"), ccv.m4get("t2"), m_ham.m2get(f_oo), m_ham.m2get(f_ov),
+    auto if2_oo = ccsd_if2_oo(ccv.m2get(t1), ccv.m4get(t2), m_ham.m2get(f_oo), m_ham.m2get(f_ov),
                     m_ham.m4get(i_ooov), m_ham.m4get(i_oovv));    
-    auto tau = ccsd_tau(ccv.m2get("t1"), ccv.m4get("t2"));
-    auto iw_oooo = ccsd_iw_oooo(ccv.m2get("t1"), tau,   
+    auto tau = ccsd_tau(ccv.m2get(t1), ccv.m4get(t2));
+    auto iw_oooo = ccsd_iw_oooo(ccv.m2get(t1), tau,   
                     m_ham.m4get(i_oooo), m_ham.m4get(i_ooov), m_ham.m4get(i_oovv));
-    auto iw_vvvv = ccsd_iw_vvvv(ccv.m2get("t1"), 
+    auto iw_vvvv = ccsd_iw_vvvv(ccv.m2get(t1), 
                     m_ham.m4get(i_ovvv), m_ham.m4get(i_vvvv));
-    auto iw_ovov = ccsd_iw_ovov(ccv.m2get("t1"), ccv.m4get("t2"), 
+    auto iw_ovov = ccsd_iw_ovov(ccv.m2get(t1), ccv.m4get(t2), 
                     m_ham.m4get(i_ooov), m_ham.m4get(i_oovv), m_ham.m4get(i_ovov), m_ham.m4get(i_ovvv));
                          
-    auto t1_new = ccsd_t1(ccv.m2get("t1"), ccv.m4get("t2"), m_ham.m2get(f_ov), if_oo, if_ov, if_vv, 
+    auto t1_new = ccsd_t1(ccv.m2get(t1), ccv.m4get(t2), m_ham.m2get(f_ov), if_oo, if_ov, if_vv, 
                     m_ham.m4get(i_ooov),m_ham.m4get(i_ovov), m_ham.m4get(i_ovvv));
-    auto t2_new = ccsd_t2(ccv.m2get("t1"), ccv.m4get("t2"), tau, if2_oo, if_vv, 
+    auto t2_new = ccsd_t2(ccv.m2get(t1), ccv.m4get(t2), tau, if2_oo, if_vv, 
                     m_ham.m4get(i_ooov), m_ham.m4get(i_oovv), m_ham.m4get(i_ovov), m_ham.m4get(i_ovvv), 
                     iw_oooo, iw_ovov, iw_vvvv);
            
-    a.set("t1", t1_new);
-    a.set("t2", t2_new);  
+    a.set(t1, t1_new);
+    a.set(t2, t2_new);  
     return value;
   }
 
