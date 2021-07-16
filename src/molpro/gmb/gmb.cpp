052736504400b47419101c184d829e1d012626e7
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2021-07-16 16:38:53 +0100

    Update DIIS

--- src/molpro/gmb/gmb.cpp
+++ src/molpro/gmb/gmb.cpp
@@ -108,7 +108,7 @@ void molpro::gmb::gmb(const molpro::Options& options) {
     problem = std::make_unique<problem_ccd>(ham);
 
   // set solver
-  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<amplitudes<>>("DIIS");
+  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<amplitudes<>>("DIIS", "max_size_qspace=10");
   auto residual = *ptampl;
 
     // solver options
