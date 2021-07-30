cef56f564789cf81bac3f2a4fb6ec35a769bf1fc
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2021-07-30 15:59:27 +0100

    Return energies to caller

--- src/molpro/gmb/gmb.cpp
+++ src/molpro/gmb/gmb.cpp
@@ -1,61 +1,67 @@
-#include "init.h"
-#include "problem_ccsd.h"
-#include "problem_ccd.h"
-#include "problem_eom-ccsd.h"
-#include "hamiltonian.h"
+#include "gmb.h"
 #include "amplitudes.h"
 #include "expressions/energy_hf.h"
+#include "hamiltonian.h"
+#include "init.h"
+#include "problem_ccd.h"
+#include "problem_ccsd.h"
+#include "problem_eom-ccsd.h"
 #include "utils.h"
-#include <memory>
-#include <molpro/linalg/itsolv/IterativeSolver.h>
-#include <molpro/linalg/itsolv/SolverFactory.h>
 #include <chrono>
 #include <ctime>
+#include <memory>
 #include <molpro/Options.h>
-#include "gmb.h"
+#include <molpro/linalg/itsolv/IterativeSolver.h>
+#include <molpro/linalg/itsolv/SolverFactory.h>
 #include <regex>
 
 using namespace gmb;
 
 extern molpro::Profiler prof;
-extern "C" void general_many_body() { molpro::gmb::gmb();}
+extern "C" void general_many_body() { molpro::gmb::gmb(); }
 
-void molpro::gmb::gmb(const molpro::Options& options) {
+std::vector<double> molpro::gmb::gmb(const molpro::Options &options) {
 
-  std::string filename = options.parameter("dump",std::string{""});
-  auto expected_results = options.parameter("results",std::vector<double>{});
-  std::vector<bool> found_expected_results(expected_results.size(),false);
+  std::vector<double> energies;
+
+  std::string filename = options.parameter("dump", std::string{""});
+  auto expected_results = options.parameter("results", std::vector<double>{});
+  std::vector<bool> found_expected_results(expected_results.size(), false);
 
   std::ios_base::sync_with_stdio(false);
   auto start = std::chrono::system_clock::now();
 
   // parse arguments
-  auto method = options.parameter("method","eom-ccsd");
+  auto method = options.parameter("method", "eom-ccsd");
   auto nroots = options.parameter("states", 3);
   auto ncav = options.parameter("polariton_modes", 0);
 
   std::vector<std::shared_ptr<polariton>> v_ppol(ncav);
   if (ncav > 0) {
-    std::vector<int> v_option_polariton_nmax(ncav, 1); 
-    v_option_polariton_nmax = options.parameter("polariton_nmax", v_option_polariton_nmax);
-    std::vector<double> v_option_polariton_gamma(ncav, 0.01); 
-    v_option_polariton_gamma = options.parameter("polariton_gamma", v_option_polariton_gamma);
+    std::vector<int> v_option_polariton_nmax(ncav, 1);
+    v_option_polariton_nmax =
+        options.parameter("polariton_nmax", v_option_polariton_nmax);
+    std::vector<double> v_option_polariton_gamma(ncav, 0.01);
+    v_option_polariton_gamma =
+        options.parameter("polariton_gamma", v_option_polariton_gamma);
     std::vector<double> v_option_polariton_omega(ncav, 1.028);
-    v_option_polariton_omega = options.parameter("polariton_omega", v_option_polariton_omega);
+    v_option_polariton_omega =
+        options.parameter("polariton_omega", v_option_polariton_omega);
     for (size_t i = 0; i < ncav; i++) {
       v_ppol[i] = std::make_shared<polariton>(v_option_polariton_nmax[i],
                                               v_option_polariton_gamma[i],
                                               v_option_polariton_omega[i]);
       v_ppol[i]->fname_dm = options.parameter(
-          "dipole", std::regex_replace(
-                        filename, std::regex{"\\.[_[:alnum:]]*$"}, ".dm"));
-      v_ppol[i]->fname_sm = std::regex_replace(
-                        filename, std::regex{"\\.[_[:alnum:]]*$"}, ".sm");
+          "dipole",
+          std::regex_replace(filename, std::regex{"\\.[_[:alnum:]]*$"}, ".dm"));
+      v_ppol[i]->fname_sm =
+          std::regex_replace(filename, std::regex{"\\.[_[:alnum:]]*$"}, ".sm");
     }
   }
 
   // print arguments
-  std::cout << "Required calculation: " << "\n\n";
+  std::cout << "Required calculation: "
+            << "\n\n";
   check_file(filename, "fcidump");
   std::cout << " fcidump = " << filename << "\n";
   std::cout << " method = " << method << "\n";
@@ -64,7 +70,8 @@ void molpro::gmb::gmb(const molpro::Options& options) {
     check_file(v_ppol[0]->fname_dm, "dipole");
     std::cout << " dipole file = " << v_ppol[0]->fname_dm << "\n";
     check_file(v_ppol[0]->fname_sm, "second moment of charges");
-    std::cout << " second moment of charges file = " << v_ppol[0]->fname_sm << "\n";
+    std::cout << " second moment of charges file = " << v_ppol[0]->fname_sm
+              << "\n";
 
     std::cout << "\nPolariton parameters: \n";
     std::cout << "modes: " << ncav << "\n";
@@ -86,13 +93,14 @@ void molpro::gmb::gmb(const molpro::Options& options) {
 
 #if 1 // CCSD
   auto vnn = get_integral(filename);
-  auto hf_energy = vnn + energy_hf(ham.m2get(f_oo),ham.m4get(i_oooo));
+  auto hf_energy = vnn + energy_hf(ham.m2get(f_oo), ham.m4get(i_oooo));
   std::cout << "HF energy: " << std::setprecision(12) << hf_energy << "\n";
 
   // set CCSD amplitudes
   if (method.find("ccs") != std::string::npos)
     ptampl->set(t1, container(ham.m2get(f_ov).get_space()));
-  if ((method.find("ccsd") != std::string::npos) || (method.find("ccd") != std::string::npos))
+  if ((method.find("ccsd") != std::string::npos) ||
+      (method.find("ccd") != std::string::npos))
     ptampl->set(t2, container(ham.m4get(i_oovv).get_space()));
 
   // set problem
@@ -102,10 +110,11 @@ void molpro::gmb::gmb(const molpro::Options& options) {
     problem = std::make_unique<problem_ccd>(ham);
 
   // set solver
-  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<amplitudes<>>("DIIS", "max_size_qspace=8");
+  auto solver = molpro::linalg::itsolv::create_NonLinearEquations<amplitudes<>>(
+      "DIIS", "max_size_qspace=8");
   auto residual = *ptampl;
 
-    // solver options
+  // solver options
   solver->set_verbosity(molpro::linalg::itsolv::Verbosity::Iteration);
   // solver->set_max_iter(110);
   // solver->set_convergence_threshold(1.0e-7);
@@ -114,12 +123,15 @@ void molpro::gmb::gmb(const molpro::Options& options) {
   problem->energy(*ptampl);
 
   // print results
-  std::cout << *problem << " correlation energy: " << std::setprecision(12) << problem->get_energy()<< "\n";
-  std::cout << *problem  << " total energy: " << std::setprecision(13)
-            << problem->get_energy() + hf_energy<< "\n";
-  for (int i=0; i<expected_results.size(); ++i)
-    if (std::abs(problem->get_energy()+hf_energy-expected_results[i])<1e-9) found_expected_results[i]=true;
-
+  std::cout << *problem << " correlation energy: " << std::setprecision(12)
+            << problem->get_energy() << "\n";
+  std::cout << *problem << " total energy: " << std::setprecision(13)
+            << problem->get_energy() + hf_energy << "\n";
+  for (int i = 0; i < expected_results.size(); ++i)
+    if (std::abs(problem->get_energy() + hf_energy - expected_results[i]) <
+        1e-9)
+      found_expected_results[i] = true;
+  energies.push_back(problem->get_energy() + hf_energy);
 
 #if 1 // Excited State
 
@@ -130,13 +142,15 @@ void molpro::gmb::gmb(const molpro::Options& options) {
   std::vector<amplitudes<>> v_rampl(nroots, *prampl);
   std::unique_ptr<problem_eom> problem_es;
   problem_es = std::make_unique<problem_eom_ccsd>(ham, *ptampl);
-  
+
   std::cout << "\n" << *problem_es << "\n";
 
-  // set solver 
-  auto solver_es = molpro::linalg::itsolv::create_LinearEigensystem<amplitudes<>>("Davidson");
+  // set solver
+  auto solver_es =
+      molpro::linalg::itsolv::create_LinearEigensystem<amplitudes<>>(
+          "Davidson");
   auto residuals_es = v_rampl;
-  
+
   // set options
   solver_es->set_verbosity(molpro::linalg::itsolv::Verbosity::Iteration);
   solver_es->set_n_roots(nroots);
@@ -145,18 +159,21 @@ void molpro::gmb::gmb(const molpro::Options& options) {
   // solve
   solver_es->solve(v_rampl, residuals_es, *problem_es, true);
   problem_es->set_energy(solver_es->eigenvalues());
-  for (const auto& ev : solver_es->eigenvalues())
-    for (int i=0; i<expected_results.size(); ++i)
-      if (std::abs(ev-expected_results[i])<1e-9) found_expected_results[i]=true;
-  auto energies = problem_es->get_energy();
+  for (const auto &ev : solver_es->eigenvalues())
+    for (int i = 0; i < expected_results.size(); ++i)
+      if (std::abs(ev - expected_results[i]) < 1e-9)
+        found_expected_results[i] = true;
+  auto excitation_energies = problem_es->get_energy();
 
   // print results
   std::cout << "\n" << *problem_es << " excitation energies (Ha) \n";
-  for (auto &i : energies)
+  for (auto &i : excitation_energies) {
     std::cout << i << " \n";
+    energies.push_back(energies.front() + i);
+  }
 
-  #endif
-  #endif
+#endif
+#endif
 
   // std::cout << *pprof << std::endl;
   auto end = std::chrono::system_clock::now();
@@ -164,12 +181,15 @@ void molpro::gmb::gmb(const molpro::Options& options) {
   std::time_t end_time = std::chrono::system_clock::to_time_t(end);
   std::cout << "\nFinished computation at " << std::ctime(&end_time)
             << "Elapsed time: " << elapsed_seconds.count() << "s\n";
-  for (int i=0; i<expected_results.size(); ++i)
+  for (int i = 0; i < expected_results.size(); ++i)
     if (not found_expected_results[i])
-      throw std::runtime_error("Did not match expected result "+std::to_string(expected_results[i]));
-
+      throw std::runtime_error("Did not match expected result " +
+                               std::to_string(expected_results[i]));
+  return energies;
 }
 
 #include <molpro/linalg/itsolv/SolverFactory-implementation.h>
-// template class molpro::linalg::itsolv::SolverFactory<amplitudes<>, amplitudes<>>;
-template class molpro::linalg::itsolv::SolverFactory<amplitudes<>, amplitudes<>>;
+// template class molpro::linalg::itsolv::SolverFactory<amplitudes<>,
+// amplitudes<>>;
+template class molpro::linalg::itsolv::SolverFactory<amplitudes<>,
+                                                     amplitudes<>>;
--- src/molpro/gmb/gmb.h
+++ src/molpro/gmb/gmb.h
@@ -1,7 +1,14 @@
 #ifndef GMB_SRC_MOLPRO_GMB_GMB_H_
 #define GMB_SRC_MOLPRO_GMB_GMB_H_
 #include <molpro/Options.h>
+#include <vector>
 namespace molpro::gmb {
-void gmb(const molpro::Options &options = molpro::Options("gmb"));
+/*!
+ * @brief Carry out gmb calculation
+ * @param options
+ * @return State energies
+ */
+std::vector<double>
+gmb(const molpro::Options &options = molpro::Options("gmb"));
 }
 #endif // GMB_SRC_MOLPRO_GMB_GMB_H_
