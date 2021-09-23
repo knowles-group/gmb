0f9b746f4642cb1787731d1815704b68cd05c33f
Marta <mrtlvdl@gmail.com>
2021-09-23 08:44:49 +0100

    shared to unique ptr

--- src/molpro/gmb/get_integral.h
+++ src/molpro/gmb/get_integral.h
@@ -52,7 +52,7 @@ double get_integral(const std::string &filename);
  * @param uhf 
  */
 void read_dump(const std::string &filename, 
-               const std::vector<std::shared_ptr<polariton>> &v_ppol,
+               const std::vector<std::unique_ptr<polariton>> &v_ppol,
                std::vector<std::vector<bool>>& v_exist,
                std::vector<std::vector<size_t>>& v_norb,
                const std::vector<orb_type>& v_orb_type, 
@@ -64,10 +64,10 @@ void read_dump(const std::string &filename,
 
 
   container<2,double> get_integral(const std::string &fname_integrals, const std::string &fname_header, 
-    const std::vector<std::shared_ptr<polariton>> &v_ppol, const orb_type &o1, const orb_type &o2, bool add_ph = true);
+    const std::vector<std::unique_ptr<polariton>> &v_ppol, const orb_type &o1, const orb_type &o2, bool add_ph = true);
   
   container<4,double> get_integral(const std::string &filename, 
-    const std::vector<std::shared_ptr<polariton>> &v_ppol,
+    const std::vector<std::unique_ptr<polariton>> &v_ppol,
     const orb_type &o1, const orb_type &o2, const orb_type &o3, const orb_type &o4);
   
   /**
@@ -81,7 +81,7 @@ void read_dump(const std::string &filename,
    * @return container<4,double> 
    */
   container<4,double> get_i(const std::string &filename, 
-               const std::vector<std::shared_ptr<polariton>> &v_ppol,
+               const std::vector<std::unique_ptr<polariton>> &v_ppol,
                const orb_type &o1, const orb_type &o2, const orb_type &o3, const orb_type &o4, const bool &add_ph = true);
 
   void get_one_electron_part(container<2,double> &integral, 
@@ -94,7 +94,7 @@ void read_dump(const std::string &filename,
                const bool &uhf);
 
   void get_one_photon_part(container<2,double> &integral, 
-               const std::vector<std::shared_ptr<polariton>> &v_ppol,
+               const std::vector<std::unique_ptr<polariton>> &v_ppol,
                const std::vector<std::vector<bool>>& v_exist,
                const std::vector<orb_type>& v_orb_type);
   
@@ -108,7 +108,7 @@ void read_dump(const std::string &filename,
                const bool &uhf);
 
  void get_electron_photon_part(container<4,double> &integral, 
-               const std::vector<std::shared_ptr<polariton>> &v_ppol,
+               const std::vector<std::unique_ptr<polariton>> &v_ppol,
                const std::vector<std::vector<bool>> &v_exist,
                const std::vector<std::vector<size_t>>& v_norb,
                const std::vector<orb_type> &v_orb_type, 
