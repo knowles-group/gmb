22ad26dfe46a5e4b4cfcd5bc0315d6e09519b69e
Marta <mrtlvdl@gmail.com>
2021-10-13 14:12:57 +0100

    add left intermediates

--- src/molpro/gmb/expressions/eom-ccsd/l1.h
+++ src/molpro/gmb/expressions/eom-ccsd/l1.h
@@ -12,6 +12,8 @@ container<2,double> eom_ccsd_l1(
     container<2, double> &l1,         ///< EOM-CCSD L1 amplitude
     container<4, double> &l2,         ///< EOM-CCSD L2 amplitude
     container<4, double> &t2,         ///< CCSD T2 amplitude
+    container<2, double> &il_oo,      ///< intermediate
+    container<2, double> &il_vv,      ///< intermediate
     container<2, double> &if_oo,      ///< intermediate - oo block
     container<2, double> &if_ov,      ///< intermediate - ov block
     container<2, double> &if_vv,      ///< intermediate - vv block
@@ -20,7 +22,6 @@ container<2,double> eom_ccsd_l1(
     container<4, double> &iw2_ooov,   ///> anti-symmetrized integral <ia||jb>
     container<4, double> &iw_ovvv,   ///> anti-symmetrized integral <ia||bc>
     container<4, double> &iw2_ovvv,   ///> anti-symmetrized integral <ia||bc>
-    container<4, double> &iw_oovv,    ///> anti-symmetrized integral <ia||bc>
     double l0 = 0.0                        ///< EOM-CCSD L0 
 
 ) {
@@ -30,17 +31,14 @@ container<2,double> eom_ccsd_l1(
     libtensor::letter i,j,m,n,o,a,b,e,f,g;
 
     l1_new(i|a) = 
-    l0*if_ov(i|a)
-                + contract(e, l1(i|e), if_vv(a|e))
-                - contract(m, l1(m|a), if_oo(m|i))
+                l0*if_ov(i|a)
+                + contract(e, l1(i|e), if_vv(e|a))
+                - contract(m, l1(m|a), if_oo(i|m))
                 - contract(e|m, l1(m|e), iw_ovov(m|a|i|e))  
-                - 0.5 * contract(e|m|n, l2(m|n|a|e), iw2_ooov(m|n|i|e))
-                + 0.5 * contract(e|f|m, l2(i|m|e|f), iw2_ovvv(m|a|f|e))
-                + 0.5 * contract(e|f, iw2_ovvv(i|e|a|f), 
-                    contract(m|n|g, l2(m|n|e|g), t2(m|n|f|g)))
-                + 0.5 * contract(m|n, iw2_ooov(i|n|m|a), 
-                    contract(e|f|o, l2(m|o|e|f), t2(n|o|e|f)))
-                    ;
+                - 0.5 * contract(e|m|n, l2(m|n|a|e), iw_ooov(m|n|i|e))
+                + 0.5 * contract(e|f|m, l2(i|m|e|f), iw_ovvv(m|a|f|e))
+                + contract(e|f, iw2_ovvv(i|e|a|f), il_vv(e|f))
+                + contract(m|n, iw2_ooov(i|n|m|a), il_oo(m|n));
 
     return l1_new;
 
--- src/molpro/gmb/expressions/eom-ccsd/l2.h
+++ src/molpro/gmb/expressions/eom-ccsd/l2.h
@@ -15,10 +15,8 @@ container<4,double> eom_ccsd_l2 (
     container<2, double> &if_oo,    ///< intermediate if_oo 
     container<2, double> &if_ov,    ///< intermediate if_ov 
     container<2, double> &if_vv,    ///< intermediate if_vv 
-    // container<2, double> &ir1_oo,   ///< intermediate ir1_oo
-    // container<2, double> &ir2_oo,   ///< intermediate ir2_oo
-    // container<2, double> &ir1_vv,   ///< intermediate ir1_vv
-    // container<2, double> &ir2_vv,   ///< intermediate ir2_vv
+    container<2, double> &il_oo,   ///< intermediate il_oo
+    container<2, double> &il_vv,   ///< intermediate il_vv
     container<4, double> &i_oovv,   ///> anti-symmetrized integral <ij||ab>
     container<4, double> &iw_oooo,  ///> intermediate iw_oooo 
     container<4, double> &iw_ooov,  ///> intermediate iw_ooov 
@@ -37,20 +35,20 @@ container<4,double> eom_ccsd_l2 (
     l2_new(i|j|a|b) = 
                     l0*i_oovv(i|j|a|b)
                       + asymm(a, b, 
-                          contract(e, l2(i|j|a|e), if_vv(b|e))
-                        - contract(m, l1(m|a), iw_ooov(i|j|m|b))
+                          contract(e, l2(i|j|a|e), if_vv(e|b))
+                        - contract(m, l1(m|a), iw2_ooov(i|j|m|b))
                         - 0.5*contract(e, i_oovv(i|j|e|b), 
                             contract(m|n|f, t2(m|n|e|f), l2(m|n|a|f))))
                      + asymm(i, j, 
-                           - contract(m, l2(i|m|a|b), if_oo(m|j))
-                           + contract(e, l1(i|e), iw_ovvv(j|e|b|a))
+                           - contract(m, l2(i|m|a|b), if_oo(j|m))
+                           + contract(e, l1(i|e), iw2_ovvv(j|e|b|a))
                         - 0.5*contract(m, i_oovv(m|j|a|b), 
                             contract(n|e|f, t2(m|n|e|f), l2(i|n|e|f)))
                         + asymm(a,b,
                             l1(i|a)*if_ov(j|b)
                          - contract(e|m, l2(i|m|a|e), iw_ovov(m|b|j|e))))
                      + 0.5 * contract(m|n, l2(m|n|a|b), iw_oooo(i|j|m|n))
-                     + 0.5 * contract(e|f, l2(i|j|e|f), iw_vvvv(a|b|e|f));
+                     + 0.5 * contract(e|f, l2(i|j|e|f), iw_vvvv(e|f|a|b));
 
     return l2_new;
 }     
